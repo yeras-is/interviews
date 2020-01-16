f408024350dfd060fb641cd392021eb6532afc7f
yeras-is <maihanov95@gmail.com>
2020-01-16 09:21:38 +0600

    cipher MagicSquare.java

---
+++ src/cipher/MagicSquare.java
@@ -0,0 +1,56 @@
+package cipher;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MagicSquare {
+    static int square[][] = {
+            {7, 12, 1, 14},
+            {2, 13, 8, 11},
+            {16, 3, 10, 5},
+            {9, 6, 15, 4}
+    };
+    static char cipher[][] = {
+            {'a', 'a', 'a', 'a'},
+            {'a', 'a', 'a', 'a'},
+            {'a', 'a', 'a', 'a'},
+            {'a', 'a', 'a', 'a'}
+    };
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Введите слово для шифрования:");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        char msg[] = word.toCharArray();
+
+        for (int i = 0; i <= msg.length - 1; i++) {
+            addChar(i + 1, msg[i]);
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int q = 0; q < cipher.length - 1; q++) {
+            builder.append(cipher[q]);
+        }
+
+        System.out.println("Зашифрованное сообщение: " + builder.toString());
+    }
+
+    static void addChar(int index, char ch) {
+        boolean found = false;
+
+        for (int i = 0; i <= square.length - 1; i++) {
+            for (int j = 0; j <= square[i].length - 1; j++) {
+                if (square[i][j] == index) {
+                    cipher[i][j] = ch;
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+    }
+
+}
--- src/cipher/Vijiner.java
+++ src/cipher/Vijiner.java
@@ -3,40 +3,43 @@ package cipher;
 import java.util.Scanner;
 
 public class Vijiner {
-        public static void main(String...s){
+    public static void main(String... s) {
 
-            System.out.println("Введите слово для шифрования:");
-            Scanner scanner = new Scanner(System.in);
-            String word = scanner.next();
-            char msg[] = word.toCharArray();
+        System.out.println("Введите слово для шифрования:");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.next();
+        System.out.println("Введите ключ:");
+        String keys = scanner.next();
 
-            char key[] = {'Ш','Е','Р','Л','О','К'};
-            int msgLen = msg.length, i, j;
+        char msg[] = word.toCharArray();
 
-            char newKey[] = new char[msgLen];
-            char encryptedMsg[] = new char[msgLen];
-            char decryptedMsg[] = new char[msgLen];
+        char key[] = keys.toCharArray();
+        int msgLen = msg.length, i, j;
 
-            //generate new key in cyclic manner equal to the length of original message
-            for(i = 0, j = 0; i < msgLen; ++i, ++j){
-                if(j == key.length)
-                    j = 0;
+        char newKey[] = new char[msgLen];
+        char encryptedMsg[] = new char[msgLen];
+        char decryptedMsg[] = new char[msgLen];
 
-                newKey[i] = key[j];
-            }
+        //generate new key in cyclic manner equal to the length of original message
+        for (i = 0, j = 0; i < msgLen; ++i, ++j) {
+            if (j == key.length)
+                j = 0;
 
-            //encryption
-            for(i = 0; i < msgLen; ++i)
-                encryptedMsg[i] = (char)(((msg[i] + newKey[i]) % 32) + 'А');
-
-            //decryption
-            for(i = 0; i < msgLen; ++i)
-                decryptedMsg[i] = (char)((((encryptedMsg[i] - newKey[i]) + 32) % 32) + 'А');
-
-            System.out.println("Original Message: " + String.valueOf(msg));     //String.valueOf() converts character array to String
-            System.out.println("Key: " + String.valueOf(key));
-            System.out.println("Generated Key: " + String.valueOf(newKey));
-            System.out.println("Encrypted Message: " + String.valueOf(encryptedMsg));
-            System.out.println("Decrypted Message: " + String.valueOf(decryptedMsg));
+            newKey[i] = key[j];
         }
+
+        //encryption
+        for (i = 0; i < msgLen; ++i)
+            encryptedMsg[i] = (char) (((msg[i] + newKey[i]) % 32) + 'А');
+
+        //decryption
+        for (i = 0; i < msgLen; ++i)
+            decryptedMsg[i] = (char) ((((encryptedMsg[i] - newKey[i]) + 32) % 32) + 'А');
+
+        System.out.println("Original Message: " + String.valueOf(msg));     //String.valueOf() converts character array to String
+        System.out.println("Key: " + String.valueOf(key));
+        System.out.println("Generated Key: " + String.valueOf(newKey));
+        System.out.println("Encrypted Message: " + String.valueOf(encryptedMsg));
+        System.out.println("Decrypted Message: " + String.valueOf(decryptedMsg));
+    }
 }
